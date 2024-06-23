04278769c344a4e159a2bde877cdc6bc14f8ad12
Udit26085 <bhatiaudit55@gmail.com>
2024-06-23 11:12:34 +0530

    updated the code according to the new UI changes

--- internshala.js
+++ internshala.js
@@ -31,6 +31,7 @@ class Internshala {
             const html = await this.__scrape_page(url);
             const page = this.__parse_page(html);
             const internships = [];
+            const $ = cheerio.load(html);
 
             const internships_container = page('.individual_internship');
 
@@ -38,12 +39,12 @@ class Internshala {
                 return { message: 'No internships found' };
             } else {
                 internships_container.each((index, element) => {
-                    const Role = page(element).find('.heading_4_5.profile').text().trim();
-                    const Company = page(element).find('.heading_6.company_name').text().trim();
+                    const Role = page(element).find('.job-internship-name').text().trim();
+                    const Company = page(element).find('.company-name').text().trim();
                     const Posted = page(element).find('.status-success').text().trim();
-                    const link_element = page(element).find('a.btn');
-                    const Link = "https://internshala.com"+link_element.attr('href');
-                    const Location = page(element).find('#location_names').text().trim();
+                    const link_element = $('.container-fluid.individual_internship.view_detail_button.visibilityTrackerItem');
+                    const Link = "https://internshala.com"+link_element.attr('data-href');
+                    const Location = page(element).find('.locations').text().trim();
                     const other_details = page(element).find('.item_body');
                     const Duration = other_details.length > 2 ? other_details.eq(1).text().trim() : 'N/A';
                     const stipend_element = page(element).find('.stipend');
@@ -76,7 +77,9 @@ class Internshala {
     }
 }
 // Example usage: Insert the role you're looking for in the function
-const search = new Internshala('');
+const search = new Internshala('web development');
 search.internships()
     .then(result => console.log(result))
     .catch(error => console.error(error));
+
+    
